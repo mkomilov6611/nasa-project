617121d15048f5d706045a7ad3853a24f32130a1
Mukhammadnazar Komilov <mkomilov6611@gmail.com>
2022-03-16 13:21:47 +0500

    add: api versioning

--- src/app.js
+++ src/app.js
@@ -1,32 +1,24 @@
 const path = require("path");
-
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const planetsRouter = require("./routes/planets/planets.router");
-const launchesRouter = require("./routes/launches/launches.router");
+const api = require("./routes/api");
 
 const app = express();
 
-// Custom Middlewares
+//Middlewares
 app.use(
   cors({
     origin: "http://localhost",
   })
 );
 app.use(morgan("combined"));
-
-// Built-in Middlewares
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-// Routes
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-});
+// Endpoints
+app.use("/v1", api);
+app.get("/*", (req, res) => res.status(404).json({ error: "Page Not Found" }));
 
 module.exports = app;
